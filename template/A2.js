bdf8f31f820d16847139fb09097c7aabbe134398
Jacob <madridjacobr@gmail.com>
2016-10-24 22:34:06 -0700

    fixed a typo that was making bad things happen

--- template/A2.js
+++ template/A2.js
@@ -9,6 +9,7 @@
 var channel = 0;
 var rotationOffsetLinkOne = 0;
 var rotationOffsetLinkTwo = 0;
+var newPose = true;
 
 function toRadians (angle) {
   return angle * (Math.PI / 180);
@@ -69,11 +70,10 @@ function getScaleMatrix(x,y,z) {
 //TODO: 
 
 function generateTentacleLinkOneMatrix(id) {
-  var parentSocket;
+  var parentSocket = null;
   var translationMatrix = getTranslationMatrix(0,baseTentacleHeight/2.0,0);
   if (id == 1) {
     parentSocket = joint1Matrix;
-
   }
   else if (id == 2) {
     parentSocket = joint2Matrix;
@@ -83,9 +83,6 @@ function generateTentacleLinkOneMatrix(id) {
   }
   else if (id == 4) {
     parentSocket = joint4Matrix;
-  }
-  else {
-    return baseTentacleMatrix;
   }
   return new THREE.Matrix4().multiplyMatrices(parentSocket, baseTentacleMatrix).multiply(translationMatrix);
   // 1. -x, 0, z
@@ -118,6 +115,7 @@ function generateTentacleLinkTwoMatrix(id) {
 function generateLinkOneJointMatrix(id) {
   var parentSocket;
   var rotationMatrix;
+  var rotationOffset = getCompositeRotation(0, 0, rotationOffsetLinkOne);
   if (id == 1) {
     parentSocket = octopusSocket1Matrix;
     rotationMatrix = getCompositeRotation(0,45,90);
@@ -137,33 +135,50 @@ function generateLinkOneJointMatrix(id) {
   else {
     return baseTentacleMatrix;
   }
-  return new THREE.Matrix4().multiplyMatrices(parentSocket, jointMatrix).multiply(rotationMatrix);
+  return new THREE.Matrix4().multiplyMatrices(parentSocket, jointMatrix).multiply(rotationMatrix).multiply(rotationOffset);
 }
 
-function poseLinkOne(angle) {
-  var rotationOffsetMatrix;
-  if (rotationOffsetLinkOne != 0) {
-    rotationOffsetMatrix = getTranslationMatrix(baseTentacleHeight/3.0,0, 0).multiply(getCompositeRotation(angle,0,angle));
-  } else {
-    rotationOffsetMatrix  = new THREE.Matrix4();
+function generateLinkTwoJointMatrix(id) {
+  var parentSocket;
+  var translationMatrix = getTranslationMatrix(0, 0.75 * baseTentacleHeight, 0);
+  var rotationOffset = getCompositeRotation(0,0,rotationOffsetLinkTwo);
+  if (id == 1) {
+    parentSocket = t_01_l_01_Matrix;
   }
-  t_01_l_01_Matrix.multiply(rotationOffsetMatrix);
-  t_02_l_01_Matrix.multiply(rotationOffsetMatrix);
-  t_03_l_01_Matrix.multiply(rotationOffsetMatrix);
-  t_04_l_01_Matrix.multiply(rotationOffsetMatrix);
+  else if (id == 2) {
+    parentSocket = t_02_l_01_Matrix;
+  }
+  else if (id == 3) {
+    parentSocket = t_03_l_01_Matrix;
+  }
+  else if (id == 4) {
+    parentSocket = t_04_l_01_Matrix;
+  }
+  else {
+    return baseTentacleMatrix;
+  }
+  return new THREE.Matrix4().multiplyMatrices(parentSocket, babbyJointMatrix).multiply(translationMatrix).multiply(rotationOffset);
 }
 
-function poseLinkTwo(angle) {
+function poseLinkOne() {
   var rotationOffsetMatrix;
-  if (rotationOffsetLinkTwo != 0) {
-    rotationOffsetMatrix = getTranslationMatrix(-baseTentacleHeight/2.0,-baseTentacleHeight/2.0,0).multiply(getCompositeRotation(angle,0,angle));
-  } else {
-    rotationOffsetMatrix = new THREE.Matrix4();
-  }
-  t_01_l_02_Matrix.multiply(rotationOffsetMatrix);
-  t_02_l_02_Matrix.multiply(rotationOffsetMatrix);
-  t_03_l_02_Matrix.multiply(rotationOffsetMatrix);
-  t_04_l_02_Matrix.multiply(rotationOffsetMatrix);
+  rotationOffsetMatrix = getCompositeRotation(rotationOffsetLinkOne, 0, rotationOffsetLinkOne);
+  joint1Matrix.multiply(rotationOffsetMatrix);
+  joint2Matrix.multiply(rotationOffsetMatrix);
+  joint3Matrix.multiply(rotationOffsetMatrix);
+  joint4Matrix.multiply(rotationOffsetMatrix);
+  updateJoints();
+}
+
+function poseLinkTwo() {
+  var rotationOffsetMatrix;
+  resetJoints();
+  rotationOffsetMatrix = getCompositeRotation(rotationOffsetLinkTwo, 0,rotationOffsetLinkTwo);
+  babbyJoint1Matrix.multiply(rotationOffsetMatrix);
+  babbyJoint2Matrix.multiply(rotationOffsetMatrix);
+  babbyJoint3Matrix.multiply(rotationOffsetMatrix);
+  babbyJoint4Matrix.multiply(rotationOffsetMatrix);
+  updateJoints();
 }
 
 function animateLinkOne() {
@@ -172,7 +187,6 @@ function animateLinkOne() {
 function animateLinkTwo() {
 
 }
-
 // ASSIGNMENT-SPECIFIC API EXTENSION
 THREE.Object3D.prototype.setMatrix = function(a) {
   this.matrix=a;
@@ -425,7 +439,7 @@ var babbyJointMatrix = new THREE.Matrix4().set(
   0.0, 0.0, 0.0, 1.0
   )
 
-var jointGeo = new THREE.SphereGeometry(0.75, 64, 64);
+var jointGeo = new THREE.SphereGeometry(0.60, 64, 64);
 
 
 var joint1Matrix = generateLinkOneJointMatrix(1);
@@ -495,10 +509,10 @@ scene.add(tentacle_04Link_01);
 
 // BABBY (link two) joints
 
-var babbyJoint1Matrix = new THREE.Matrix4().multiplyMatrices(t_01_l_01_Matrix, babbyJointMatrix).multiply(getTranslationMatrix(0, 0.75 * baseTentacleHeight, 0));
-var babbyJoint2Matrix = new THREE.Matrix4().multiplyMatrices(t_02_l_01_Matrix, babbyJointMatrix).multiply(getTranslationMatrix(0, 0.75 * baseTentacleHeight, 0));
-var babbyJoint3Matrix = new THREE.Matrix4().multiplyMatrices(t_03_l_01_Matrix, babbyJointMatrix).multiply(getTranslationMatrix(0, 0.75 * baseTentacleHeight, 0));
-var babbyJoint4Matrix = new THREE.Matrix4().multiplyMatrices(t_04_l_01_Matrix, babbyJointMatrix).multiply(getTranslationMatrix(0, 0.75 * baseTentacleHeight, 0));
+var babbyJoint1Matrix = generateLinkTwoJointMatrix(1);
+var babbyJoint2Matrix = generateLinkTwoJointMatrix(2);
+var babbyJoint3Matrix = generateLinkTwoJointMatrix(3);
+var babbyJoint4Matrix = generateLinkTwoJointMatrix(4);
 
 var tentacle1L2joint = new THREE.Mesh(jointGeo,normalMaterial);
 var tentacle2L2joint = new THREE.Mesh(jointGeo,normalMaterial);
@@ -544,6 +558,54 @@ var t_04_l_02_Matrix = generateTentacleLinkTwoMatrix(4);
 var tentacle_04Link_02 = new THREE.Mesh(tentacleL02Geom,normalMaterial);
 tentacle_04Link_02.setMatrix(t_04_l_02_Matrix);
 scene.add(tentacle_04Link_02);
+
+var defaultL1J1 = joint1Matrix;
+var defaultL1J2 = babbyJoint1Matrix;
+var defaultL2J1 = joint2Matrix;
+var defaultL2J2 = babbyJoint2Matrix;
+var defaultL3J1 = joint3Matrix;
+var defaultL3J2 = babbyJoint3Matrix;
+var defaultL4J1 = joint4Matrix;
+var defaultL4J2 = babbyJoint4Matrix;
+
+function resetJoints() {
+  joint1Matrix = defaultL1J1;
+  joint2Matrix = defaultL2J1;
+  joint3Matrix = defaultL3J1;
+  joint4Matrix = defaultL4J1;
+  tentacle1L1joint.setMatrix(joint1Matrix);
+  tentacle2L1joint.setMatrix(joint2Matrix);
+  tentacle3L1joint.setMatrix(joint3Matrix);
+  tentacle4L1joint.setMatrix(joint4Matrix);
+  babbyJoint1Matrix = defaultL1J2;
+  babbyJoint2Matrix = defaultL2J2;
+  babbyJoint3Matrix = defaultL3J2;
+  babbyJoint4Matrix = defaultL4J2;
+  tentacle1L2joint.setMatrix(babbyJoint1Matrix);
+  tentacle2L2joint.setMatrix(babbyJoint2Matrix);
+  tentacle3L2joint.setMatrix(babbyJoint3Matrix);
+  tentacle4L2joint.setMatrix(babbyJoint4Matrix);
+
+}
+
+function updateJoints() {
+  joint1Matrix = generateLinkOneJointMatrix(1);
+  joint2Matrix = generateLinkOneJointMatrix(2);
+  joint3Matrix = generateLinkOneJointMatrix(3);
+  joint4Matrix = generateLinkOneJointMatrix(4);
+  tentacle1L1joint.setMatrix(joint1Matrix);
+  tentacle2L1joint.setMatrix(joint2Matrix);
+  tentacle3L1joint.setMatrix(joint3Matrix);
+  tentacle4L1joint.setMatrix(joint4Matrix);
+  babbyJoint1Matrix = generateLinkTwoJointMatrix(1);
+  babbyJoint2Matrix = generateLinkTwoJointMatrix(2);
+  babbyJoint3Matrix = generateLinkTwoJointMatrix(3);
+  babbyJoint4Matrix = generateLinkTwoJointMatrix(4);
+  tentacle1L2joint.setMatrix(babbyJoint1Matrix);
+  tentacle2L2joint.setMatrix(babbyJoint2Matrix);
+  tentacle3L2joint.setMatrix(babbyJoint3Matrix);
+  tentacle4L2joint.setMatrix(babbyJoint4Matrix);
+}
 
 function resetLinks() {
   t_01_l_01_Matrix = generateTentacleLinkOneMatrix(1);
@@ -604,20 +666,15 @@ function updateBody() {
   switch(channel)
   {
     //add poses here:
-    case 0: 
-      rotationOffsetLinkOne = 0;
-      rotationOffsetLinkTwo = 0;
-      updateFirstLink();
-      updateSecondLink();
+    case 0:
+      resetJoints();
       onTheClock = false;
       // DEFAULT "T-POSE"
       break;
 
     case 1:
-      rotationOffsetLinkOne = -45;
-      rotationOffsetLinkTwo = 90;
-      updateFirstLink();
-      updateSecondLink();
+      tentacle1L1joint.multiply(getCompositeRotation(0,0,1));
+      updateJoints();
       onTheClock = false;
       // SPIDER MODE
       break;
@@ -625,8 +682,7 @@ function updateBody() {
     case 2:
       rotationOffsetLinkOne = 0;
       rotationOffsetLinkTwo = 90;
-      updateFirstLink();
-      updateSecondLink();
+      updateJoints();
       onTheClock = false;
       break;
 
@@ -634,6 +690,7 @@ function updateBody() {
     //animation
     case 3:  
       {
+        /*
         var timeslice = animThresh / 4.0;
         if (!onTheClock) {
           rotationOffsetLinkOne = 0;
@@ -657,7 +714,7 @@ function updateBody() {
         }
         //animate octopus here:
         updateFirstLink();
-        updateSecondLink();
+        updateSecondLink(); */
       }
 
       break;
